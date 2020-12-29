badf1185c8ad00ed1a31db8718efab39fb828f2f
Yan-pg <yancesa.07@gmail.com>
2020-12-29 14:36:40 -0300

    fet: Fist commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ResponsiveLine } from '@nivo/line';
+import React from 'react';
+import './main.css'
+import data from '../src/pages/chart/data.json'
+import Chart from './pages/chart';
+
+
+const App: React.FC = () => (
+  <Chart />
+)
+
+export default App
+
---
+++ src/pages/chart/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LineChart from './lineChart';
+import './styles.css'
+
+const Chart: React.FC = () => {
+  return (
+      <div className="grafic">
+          <LineChart />
+      </div>
+  )
+}
+
+export default Chart
